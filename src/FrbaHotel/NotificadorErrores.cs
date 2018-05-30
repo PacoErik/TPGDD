159a3b49f881fae6dbac1bf399c5a55f2283b826
PacoErik <erikpacocolque@est.frba.utn.edu.ar>
2018-05-30 00:02:11 -0300

    [NotificadorErrores] Se agrega una clase para construir un texto con los distintos errores que puede haber en un formulario

---
+++ src/FrbaHotel/NotificadorErrores.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrbaHotel
+{
+    public class NotificadorErrores
+    {
+        String errores;
+        public NotificadorErrores() 
+        {
+            errores = "";
+        }
+        public void agregarError(String textoError)
+        {
+            errores += "\n- " + textoError;
+        }
+        public void mostrarErrores()
+        {
+            MessageBox.Show("Debe corregir lo siguiente:"+errores);
+        }
+        public bool hayError()
+        {
+            return !errores.Equals("");
+        }
+    }
+}
