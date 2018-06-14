1cef7ca5b0197e328807b7f13b9eb5e4d92ba409
PacoErik <erikpacocolque@est.frba.utn.edu.ar>
2018-06-13 21:44:48 -0300

    [Main] Un poco más bonito

--- src/FrbaHotel/Main.Designer.cs
+++ src/FrbaHotel/Main.Designer.cs
@@ -34,12 +34,15 @@
             this.fechaSistema = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.cambiarFecha = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Huesped
             // 
-            this.Huesped.Location = new System.Drawing.Point(69, 77);
+            this.Huesped.Location = new System.Drawing.Point(69, 126);
             this.Huesped.Name = "Huesped";
             this.Huesped.Size = new System.Drawing.Size(150, 38);
             this.Huesped.TabIndex = 0;
@@ -49,7 +52,7 @@
             // 
             // Usuario
             // 
-            this.Usuario.Location = new System.Drawing.Point(69, 167);
+            this.Usuario.Location = new System.Drawing.Point(69, 184);
             this.Usuario.Name = "Usuario";
             this.Usuario.Size = new System.Drawing.Size(150, 38);
             this.Usuario.TabIndex = 1;
@@ -97,11 +100,47 @@
             this.cambiarFecha.UseVisualStyleBackColor = true;
             this.cambiarFecha.Click += new System.EventHandler(this.cambiarFecha_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 28);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Sistema de";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(94, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 28);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Gestión";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(174, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 28);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Hotelera";
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(281, 382);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.cambiarFecha);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.Salir);
@@ -112,6 +151,7 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -123,6 +163,9 @@
         private System.Windows.Forms.Label fechaSistema;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button cambiarFecha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
