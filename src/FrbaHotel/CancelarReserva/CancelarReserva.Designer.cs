1fd3cd8ca48372e57e8e99db2a123f92419f2bd8
isarasibar <sarasibar.ignacio@gmail.com>
2018-06-27 21:21:23 -0300

    Cancelar reservas corregido

--- src/FrbaHotel/CancelarReserva/CancelarReserva.Designer.cs
+++ src/FrbaHotel/CancelarReserva/CancelarReserva.Designer.cs
@@ -31,13 +31,14 @@
             this.txtbox_codigo = new System.Windows.Forms.TextBox();
             this.txtbox_motivo = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.atras = new System.Windows.Forms.Button();
             this.enviar = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.btn_cargar_reserva = new System.Windows.Forms.Button();
             this.lbl_error = new System.Windows.Forms.Label();
             this.lbl_ingrese_motivo = new System.Windows.Forms.Label();
             this.lbl_cargado_correcto = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelMotivo = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtbox_codigo
@@ -49,7 +50,9 @@
             // 
             // txtbox_motivo
             // 
-            this.txtbox_motivo.Location = new System.Drawing.Point(60, 109);
+            this.txtbox_motivo.Enabled = false;
+            this.txtbox_motivo.Location = new System.Drawing.Point(60, 142);
+            this.txtbox_motivo.MaxLength = 255;
             this.txtbox_motivo.Multiline = true;
             this.txtbox_motivo.Name = "txtbox_motivo";
             this.txtbox_motivo.Size = new System.Drawing.Size(212, 148);
@@ -58,27 +61,16 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 109);
+            this.label2.Location = new System.Drawing.Point(16, 142);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(42, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Motivo:";
             // 
-            // atras
-            // 
-            this.atras.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.atras.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.atras.Location = new System.Drawing.Point(12, 271);
-            this.atras.Name = "atras";
-            this.atras.Size = new System.Drawing.Size(62, 34);
-            this.atras.TabIndex = 6;
-            this.atras.Text = "Atrás";
-            this.atras.UseVisualStyleBackColor = false;
-            this.atras.Click += new System.EventHandler(this.atras_Click);
-            // 
             // enviar
             // 
-            this.enviar.Location = new System.Drawing.Point(197, 278);
+            this.enviar.Enabled = false;
+            this.enviar.Location = new System.Drawing.Point(197, 305);
             this.enviar.Name = "enviar";
             this.enviar.Size = new System.Drawing.Size(75, 23);
             this.enviar.TabIndex = 7;
@@ -97,7 +89,7 @@
             // 
             // btn_cargar_reserva
             // 
-            this.btn_cargar_reserva.Location = new System.Drawing.Point(90, 278);
+            this.btn_cargar_reserva.Location = new System.Drawing.Point(173, 67);
             this.btn_cargar_reserva.Name = "btn_cargar_reserva";
             this.btn_cargar_reserva.Size = new System.Drawing.Size(99, 23);
             this.btn_cargar_reserva.TabIndex = 9;
@@ -109,44 +101,69 @@
             // 
             this.lbl_error.AutoSize = true;
             this.lbl_error.ForeColor = System.Drawing.Color.Red;
-            this.lbl_error.Location = new System.Drawing.Point(39, 48);
+            this.lbl_error.Location = new System.Drawing.Point(12, 49);
             this.lbl_error.Name = "lbl_error";
             this.lbl_error.Size = new System.Drawing.Size(46, 13);
             this.lbl_error.TabIndex = 10;
             this.lbl_error.Text = "ERROR";
+            this.lbl_error.Visible = false;
             // 
             // lbl_ingrese_motivo
             // 
             this.lbl_ingrese_motivo.AutoSize = true;
             this.lbl_ingrese_motivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
-            this.lbl_ingrese_motivo.Location = new System.Drawing.Point(70, 90);
+            this.lbl_ingrese_motivo.Location = new System.Drawing.Point(57, 123);
             this.lbl_ingrese_motivo.Name = "lbl_ingrese_motivo";
             this.lbl_ingrese_motivo.Size = new System.Drawing.Size(191, 16);
             this.lbl_ingrese_motivo.TabIndex = 11;
-            this.lbl_ingrese_motivo.Text = "Ingrese motivo de cancelacion";
+            this.lbl_ingrese_motivo.Text = "Ingrese motivo de cancelación";
+            this.lbl_ingrese_motivo.Visible = false;
             // 
             // lbl_cargado_correcto
             // 
             this.lbl_cargado_correcto.AutoSize = true;
             this.lbl_cargado_correcto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
-            this.lbl_cargado_correcto.Location = new System.Drawing.Point(70, 74);
+            this.lbl_cargado_correcto.Location = new System.Drawing.Point(111, 41);
             this.lbl_cargado_correcto.Name = "lbl_cargado_correcto";
             this.lbl_cargado_correcto.Size = new System.Drawing.Size(114, 16);
             this.lbl_cargado_correcto.TabIndex = 12;
             this.lbl_cargado_correcto.Text = "Reserva cargada";
+            this.lbl_cargado_correcto.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Cancelar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonCancelar_Click);
+            // 
+            // labelMotivo
+            // 
+            this.labelMotivo.AutoSize = true;
+            this.labelMotivo.ForeColor = System.Drawing.Color.Red;
+            this.labelMotivo.Location = new System.Drawing.Point(57, 110);
+            this.labelMotivo.Name = "labelMotivo";
+            this.labelMotivo.Size = new System.Drawing.Size(155, 13);
+            this.labelMotivo.TabIndex = 14;
+            this.labelMotivo.Text = "El motivo no puede estar vacío";
+            this.labelMotivo.Visible = false;
             // 
             // CancelarReserva
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 313);
+            this.ClientSize = new System.Drawing.Size(284, 340);
+            this.Controls.Add(this.labelMotivo);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lbl_cargado_correcto);
             this.Controls.Add(this.lbl_ingrese_motivo);
             this.Controls.Add(this.lbl_error);
             this.Controls.Add(this.btn_cargar_reserva);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.enviar);
-            this.Controls.Add(this.atras);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtbox_motivo);
             this.Controls.Add(this.txtbox_codigo);
@@ -162,13 +179,14 @@
         private System.Windows.Forms.TextBox txtbox_codigo;
         private System.Windows.Forms.TextBox txtbox_motivo;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button atras;
         private System.Windows.Forms.Button enviar;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btn_cargar_reserva;
         private System.Windows.Forms.Label lbl_error;
         private System.Windows.Forms.Label lbl_ingrese_motivo;
         private System.Windows.Forms.Label lbl_cargado_correcto;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelMotivo;
 
     }
 }
--- src/FrbaHotel/CancelarReserva/CancelarReserva.cs
+++ src/FrbaHotel/CancelarReserva/CancelarReserva.cs
@@ -13,59 +13,50 @@ namespace FrbaHotel.CancelarReserva
 {
     public partial class CancelarReserva : Form
     {
-        private int usuario;
-        private int hotel;
+        private long usuario;
+        private long hotel;
         private string codigo;
         private string tipo_cancelacion;
-
-        private SqlCommand command;
         private DataTable reserva = new DataTable();
-        private DataTable estados_reserva = new DataTable();
-        public CancelarReserva(int userId, string hotelId)
+        public CancelarReserva(long userId, string hotelId)
         {
             InitializeComponent();
             UtilesSQL.inicializar();
             usuario = userId;
-            hotel = Convert.ToInt32(hotelId);
-            lbl_error.Visible = false;
-            lbl_cargado_correcto.Visible = false;
-            lbl_ingrese_motivo.Visible = false;
-            txtbox_motivo.Enabled = false;
+            hotel = Convert.ToInt64(hotelId);
             SetearTipoCancelacion();
-            CargarEstadosReserva();
-        }
-
-        private void CargarEstadosReserva()
-        {
-            estados_reserva = new DataTable();
-            UtilesSQL.llenarTabla(estados_reserva, "SELECT * FROM DERROCHADORES_DE_PAPEL.EstadoDeReserva");
         }
 
         private void SetearTipoCancelacion()
         { 
-            if(usuario == GetUsuarioGuest())
+            //Se fija si el usuario es recepción o un "guest"
+            if(usuario == 2)
             {
                 tipo_cancelacion = "RESERVA CANCELADA POR CLIENTE";
-            }else{
+            }
+            else
+            {
                 tipo_cancelacion = "RESERVA CANCELADA POR RECEPCION";
             }
         }
 
-        private int GetUsuarioGuest()
-        {
-            DataTable user = new DataTable();
-            UtilesSQL.llenarTabla(user, "SELECT * FROM DERROCHADORES_DE_PAPEL.Usuario WHERE usur_username = 'guest'");
-            return Convert.ToInt32(user.Rows[0]["usur_id"]);
-        }
-
         private void btn_cargar_reserva_Click(object sender, EventArgs e)
         {
-            if(txtbox_codigo.Text != "")
+            if (String.IsNullOrWhiteSpace(txtbox_codigo.Text))
             {
-                CargarReserva(txtbox_codigo.Text);
+                lbl_error.Text = "Por favor ingrese una reserva";
+                lbl_error.Visible = true;
+                return;
             }
+            double num;
+            if (!double.TryParse(txtbox_codigo.Text, out num))
+            {
+                lbl_error.Text = "Reserva inválida";
+                lbl_error.Visible = true;
+                return;
+            }
+            CargarReserva(txtbox_codigo.Text);
         }
-
         private void CargarReserva(string codigo_reserva)
         {
             reserva = new DataTable();
@@ -74,70 +65,82 @@ namespace FrbaHotel.CancelarReserva
             {
                 lbl_error.Text = "No se encuentra la reserva";
                 lbl_error.Visible = true;
+                return;
             }
-            else {
-                if (Convert.ToInt32(reserva.Rows[0]["rese_hotel"]) != hotel)
-                {
-                    lbl_error.Text = "La reserva correspondea otro hotel";
-                    lbl_error.Visible = true;
-                }
-                else
-                {
-                    TimeSpan dias_para_reserva = Convert.ToDateTime(Main.fecha()) - Convert.ToDateTime(reserva.Rows[0]["rese_inicio"]);
-                    if (dias_para_reserva.Duration().Days <= 1)
-                    {
-                        lbl_error.Text = "No se puede cancelar a menos de un dia";
-                        lbl_error.Visible = true;
-                    }
-                    else
-                    {
-                        if (!ReservaYaCancelada())
-                        {
-                            lbl_error.Visible = false;
-                            codigo = txtbox_codigo.Text;
-                            lbl_cargado_correcto.Visible = true;
-                            lbl_ingrese_motivo.Visible = true;
-                            txtbox_motivo.Enabled = true;
-                        }
-                        else
-                        {
-                            lbl_error.Text = "Ya se cancelo esta reserva";
-                            lbl_error.Visible = true;
-                        }
-                    }
-                }
-            }                
+            if (Convert.ToInt64(reserva.Rows[0]["rese_hotel"]) != hotel)
+            {
+                lbl_error.Text = "La reserva corresponde a otro hotel";
+                lbl_error.Visible = true;
+                return;
+            }
+            if (ReservaYaCancelada())
+            {
+                lbl_error.Text = "Ya se canceló esta reserva";
+                lbl_error.Visible = true;
+                return;
+            }
+            if (ReservaEfectivizada())
+            {
+                lbl_error.Text = "Esta reserva ya se efectivizó";
+                lbl_error.Visible = true;
+                return;
+            }
+            if (Convert.ToDateTime(Main.fecha()) > Convert.ToDateTime(reserva.Rows[0]["rese_inicio"]))
+            {
+                lbl_error.Text = "La reserva ya caducó";
+                lbl_error.Visible = true;
+                return;
+            }
+            TimeSpan dias_para_reserva = Convert.ToDateTime(Main.fecha()) - Convert.ToDateTime(reserva.Rows[0]["rese_inicio"]);
+            if (dias_para_reserva.Duration().Days <= 1)
+            {
+                lbl_error.Text = "No se puede cancelar a menos de un día";
+                lbl_error.Visible = true;
+                return;
+            }
+            txtbox_codigo.ReadOnly = true;
+            btn_cargar_reserva.Enabled = false;
+            lbl_error.Visible = false;
+            codigo = txtbox_codigo.Text;
+            lbl_cargado_correcto.Visible = true;
+            lbl_ingrese_motivo.Visible = true;
+            txtbox_motivo.Enabled = true;
+            enviar.Enabled = true;
         }
-
         private bool ReservaYaCancelada()
         {
             int estado_actual = Convert.ToInt32(reserva.Rows[0]["rese_estado"]);
             return (estado_actual == getEstadosDeReserva("RESERVA CANCELADA POR RECEPCION")) || (estado_actual == getEstadosDeReserva("RESERVA CANCELADA POR CLIENTE")) || (estado_actual == getEstadosDeReserva("RESERVA CANCELADA POR NO-SHOW"));
         }
+        private bool ReservaEfectivizada()
+        {
+            int estado_actual = Convert.ToInt32(reserva.Rows[0]["rese_estado"]);
+            return (estado_actual == getEstadosDeReserva("RESERVA EFECTIVIZADA"));
+        }
 
         private void enviar_Click(object sender, EventArgs e)
         {
-            if (txtbox_motivo.Text != "")
+            //txtbox_motivo tiene MaxLength = 255, así que no hay que verificar eso 
+            labelMotivo.Visible = false;
+            if (!String.IsNullOrWhiteSpace(txtbox_motivo.Text))
             {
-                if (txtbox_motivo.Text.Length > 255)
-                {
-                    MessageBox.Show("Motivo muy largo");
-                }
-                else {
-                    command = UtilesSQL.crearCommand("INSERT INTO DERROCHADORES_DE_PAPEL.CancelacionReserva (canc_reserva, canc_motivo, canc_fechaDeCancelacion, canc_usuario) VALUES (@reserva, @motivo, @fecha, @user)");
-                    command.Parameters.AddWithValue("@reserva", Convert.ToInt32(codigo));
-                    command.Parameters.AddWithValue("@motivo", txtbox_motivo.Text);
-                    command.Parameters.AddWithValue("@fecha", Convert.ToDateTime(Main.fecha()));
-                    command.Parameters.AddWithValue("@user", usuario);
-                    UtilesSQL.ejecutarComandoNonQuery(command);
+                SqlCommand command = UtilesSQL.crearCommand("INSERT INTO DERROCHADORES_DE_PAPEL.CancelacionReserva (canc_reserva, canc_motivo, canc_fechaDeCancelacion, canc_usuario) VALUES (@reserva, @motivo, CONVERT(DATETIME, @fecha), @user)");
+                command.Parameters.AddWithValue("@reserva", Convert.ToInt64(codigo));
+                command.Parameters.AddWithValue("@motivo", txtbox_motivo.Text);
+                command.Parameters.AddWithValue("@fecha", Main.fecha());
+                command.Parameters.AddWithValue("@user", usuario);
+                UtilesSQL.ejecutarComandoNonQuery(command);
 
-                    command = UtilesSQL.crearCommand("UPDATE DERROCHADORES_DE_PAPEL.Reserva SET rese_estado = @estado WHERE rese_codigo = @reserva");
-                    command.Parameters.AddWithValue("@reserva", Convert.ToInt32(codigo));
-                    command.Parameters.AddWithValue("@estado", getEstadosDeReserva(tipo_cancelacion));
-                    UtilesSQL.ejecutarComandoNonQuery(command);
-                    MessageBox.Show("Se elimino la reserva " + codigo);
-                    Close();
-                }
+                SqlCommand command2 = UtilesSQL.crearCommand("UPDATE DERROCHADORES_DE_PAPEL.Reserva SET rese_estado = @estado WHERE rese_codigo = @reserva");
+                command2.Parameters.AddWithValue("@reserva", Convert.ToInt64(codigo));
+                command2.Parameters.AddWithValue("@estado", getEstadosDeReserva(tipo_cancelacion));
+                UtilesSQL.ejecutarComandoNonQuery(command2);
+                MessageBox.Show("Se canceló la reserva " + codigo);
+                Close();
+            }
+            else
+            {
+                labelMotivo.Visible = true;
             }
         }
 
@@ -148,12 +151,9 @@ namespace FrbaHotel.CancelarReserva
             return Convert.ToInt32(estados_reserva.Rows[0]["esta_id"]);
         }
 
-        private void atras_Click(object sender, EventArgs e)
+        private void buttonCancelar_Click(object sender, EventArgs e)
         {
             Close();
         }
-
-
-
     }
 }
